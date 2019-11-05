03d48b30b9e227ccf05e2f398a9e67259583b611
claudeisakeeb <47193525+claudeisakeeb@users.noreply.github.com>
2019-11-05 11:32:58 -0800

    Updated G2 Website

---
+++ G2/script.js
@@ -0,0 +1,7 @@
+function changeImg(name){
+  document.getElementById(name).src = "./img/"+name+"h.png"
+}
+
+function changeImgBack(name){
+  document.getElementById(name).src = "./img/"+name+".png"
+}
